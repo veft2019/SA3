d8c8c6b1a9589f5906fec6cd3d8aadfbd0fdbe02
HjorturJ <hjorturv17@ru.is>
2019-10-01 20:38:22 +0000

    Added request package and worked on the hit function some more

--- services/pinataService.js
+++ services/pinataService.js
@@ -1,4 +1,5 @@
 const dbProvider = require('../data/data');
+const request = require('request');
 
 const pinataService = () => {
     const getAllPinatas = () => {
@@ -26,18 +27,22 @@ const pinataService = () => {
         result.currentHits++;
 
         if(result.currentHits == result.maximumHits) {
-            //Use surprise property and check if its text or url
-            //Add to text file (or create one if one doesnt exist)
-            //or
-            //Download image from url using request write stream
+            if(result.surprise.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+                //Gotta find what type the img is and add that to the end of the file name
+                //Just some string formatt stuff
+                request(result.surprise).pipe(fs.createWriteStream('test.jpg'));
 
+            }
+            else {
+                //Append to txt file
+            }
             //Return 200 along with the surprise (text or url, doesnt matter in the return function)
         }
         else if(result.currentHits > result.maximumHits) {
             //Return 423
         }
             
-        //if we get here we must return 204
+        //if we get here we must return 204 with no body
         return result;
     }
 
